ef3089802c4d282e3c0b8c2508d328fa3d99d8a2
Geetikapandey5 <geetikapandey5082001@gmail.com>
2024-07-24 21:15:34 +0530

    success

---
+++ config/allowedOrigins.js
@@ -0,0 +1,10 @@
+const allowedOrigins = [
+    'http://localhost:5175',
+    'http://localhost:4200',
+    'http://localhost:5176' ,
+    'http://localhost:5173' ,
+    'http://localhost:5174' ,
+    'http://localhost:5177' 
+];
+
+module.exports = allowedOrigins;
